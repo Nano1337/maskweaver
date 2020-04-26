6dfc8ec34419339550aabf3bc0150e2230ff978a
Daniel Wei <dominum15@outlook.com>
2020-04-26 07:41:58 -0500

    Click to copy ID

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -4,36 +4,48 @@ import { AuthUserContext, withAuthorization } from '../Session';
 import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
 import SignOutButton from "../SignOut";
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
-const AccountPage = () => (
-    <div className = "page">
-        <AuthUserContext.Consumer>
-            {authUser => (
-                <div>
-                    <div className="colorheader">
-                            <h1>Account</h1>
-                            <h3>{authUser.email}</h3>
-                            <h3>Points: {/* TODO: put points here, maybe call for authUser?? */}</h3>
-                            <h5>ID: {authUser.uid}</h5>
-                            <SignOutButton />
-                    </div>   
-                    <br /><br /><hr /><br />
-                    <center>
+function AccountPage(props) {
+    const [copyMsg, setCopyMsg] = React.useState("tap to copy");
+
+    return (
+        <div className = "page">
+            <AuthUserContext.Consumer>
+                {authUser => (
+                    <div>
                         <div className="colorheader">
-                            <h2>Change Password</h2>
-                        </div>
-                        
-                        <PasswordChangeForm />
-                    </center>                 
-                    
-                </div>
-            )}
-        </AuthUserContext.Consumer>
-        <p></p>
-       
-    </div>
+                                <h1>Account</h1>
+                                <h3>{authUser.email}</h3>
+                                <h3>Points: {/* TODO: put points here, maybe call for authUser?? */}</h3>
+                                
+                                <CopyToClipboard text={authUser.uid} onCopy={() => setCopyMsg("Copied")}>
+                                    <div>
+                                        <span><b>Your ID </b>({copyMsg}):</span><br />
+                                        <span>{authUser.uid}</span>
+                                    </div>
+                                </CopyToClipboard>
 
-);
+                                <br /><br />
+                                <SignOutButton />
+                        </div>   
+                        <br /><br /><hr /><br />
+                        <center>
+                            <div className="colorheader">
+                                <h2>Change Password</h2>
+                            </div>
+                            
+                            <PasswordChangeForm />
+                        </center>                 
+                        
+                    </div>
+                )}
+            </AuthUserContext.Consumer>
+            <p></p>
+        
+        </div>
+    );
+}
 
 const condition = authUser => !!authUser;
 
